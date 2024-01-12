f7a82c47ae37cbd3cc26c73e7726b895351e103b
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-12 09:19:21 +0100

    added config for absolute import paths

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Layout from './components/Layout';
-import Error from './components/Error';
-import Home from './pages/Home';
+import Layout from '@/components/Layout';
+import Error from '@/components/Error';
+import Home from '@/pages/Home';
+import Products from '@/pages/Products';
+import About from '@/pages/About';
+import Contact from '@/pages/Contact';
+import Product from '@/pages/Product';
 
 const router = createBrowserRouter([
   {
@@ -14,18 +18,22 @@ const router = createBrowserRouter([
         element: <Home />,
         index: true,
       },
-      // {
-      //   path: '/search',
-      //   element: <SearchPage />,
-      // },
-      // {
-      //   path: '/contact',
-      //   element: <Contact />,
-      // },
-      // {
-      //   path: '/ad/:id',
-      //   element: <Ad />,
-      // },
+      {
+        path: '/products',
+        element: <Products />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
     ],
   },
   {
@@ -37,10 +45,6 @@ const router = createBrowserRouter([
         path: '',
         element: <p>ADMIN</p>,
         index: true,
-      },
-      {
-        path: '/admin/search',
-        element: <p>ADMIN SEARCH</p>,
       },
     ],
   },
