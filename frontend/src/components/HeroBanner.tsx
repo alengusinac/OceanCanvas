ef341e5112aab05da01c033e9e4ec01f752767a7
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-12 13:13:01 +0100

    added HeroBanner component

--- frontend/src/components/HeroBanner.tsx
+++ frontend/src/components/HeroBanner.tsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 interface Props {
   image: string;
+  buttonText: string;
+  onClick: React.MouseEventHandler;
 }
 
 const HeroWrapper = styled.div`
   width: 100%;
   height: 250px;
   overflow: hidden;
+  position: relative;
 `;
 
 const Image = styled.img`
@@ -16,9 +21,19 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-const HeroBanner = ({ image }: Props) => {
+const HeroBanner = ({ image, buttonText, onClick }: Props) => {
   return (
     <HeroWrapper>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ position: 'absolute', width: '100%', height: '100%' }}
+      >
+        <Button variant="contained" onClick={onClick} sx={{ height: 40 }}>
+          {buttonText}
+        </Button>
+      </Box>
       <Image src={image} />
     </HeroWrapper>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,32 @@
 import HeroBanner from '@/components/HeroBanner';
 import ocean1 from '@/assets/images/ocean1.jpg';
+import ocean2 from '@/assets/images/ocean2.jpg';
+import ocean3 from '@/assets/images/ocean3.jpg';
+
+const handleClick = () => {
+  console.log('CLICK');
+};
 
 const Home = () => {
-  return <HeroBanner image={ocean1} />;
+  return (
+    <>
+      <HeroBanner
+        image={ocean1}
+        buttonText="Explore Prints"
+        onClick={handleClick}
+      />
+      <HeroBanner
+        image={ocean2}
+        buttonText="Explore Fish"
+        onClick={handleClick}
+      />
+      <HeroBanner
+        image={ocean3}
+        buttonText="Explore Corals"
+        onClick={handleClick}
+      />
+    </>
+  );
 };
 
 export default Home;
