bee54a97992f4745653056c68dce4596fad98dbe
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-17 15:26:44 +0100

    added basic About page

---
+++ frontend/src/components/styled/About.styled.ts
@@ -0,0 +1,10 @@
+import { colors } from '@/styles/variables';
+import styled from 'styled-components';
+
+export const StyledAbout = styled.div`
+  background-color: ${colors.white};
+
+  img {
+    width: 150px;
+  }
+`;
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,5 +1,22 @@
+import photographer1 from '@/assets/images/oceanPhotographer.jpg';
+import photographer2 from '@/assets/images/oceanPhotographer2.jpg';
+import { StyledAbout } from '@/components/styled/About.styled';
+
 const About = () => {
-  return <p>About</p>;
+  return (
+    <StyledAbout>
+      <h1>About Us</h1>
+      <p>
+        Welcome to OceanCanvas, where the mesmerizing world beneath the waves
+        comes to life through the lens of the exceptionally talented
+        photographer, Alen Gusinac. Our journey began with Alen's profound
+        appreciation for the beauty hidden beneath the ocean's surface, and a
+        shared desire to bring that beauty to the world.
+      </p>
+      <img src={photographer1} alt="" />
+      <img src={photographer2} alt="" />
+    </StyledAbout>
+  );
 };
 
 export default About;
