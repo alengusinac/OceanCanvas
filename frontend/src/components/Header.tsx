e028134b0c9f96deefca39266e1781674a27c05c
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-17 13:53:52 +0100

    added basic Drawer with nav links

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,17 +5,55 @@ import {
   MdOutlineShoppingCart,
   MdMenu,
 } from 'react-icons/md';
-import { HeaderWrapper, Logo } from './styled/Header.styled';
+import { HeaderWrapper, Logo, StyledNav } from './styled/Header.styled';
 import { FlexWrapper } from './styled/Flex.styled';
+import Drawer from '@mui/material/Drawer';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Divider from '@mui/material/Divider';
+useNavigate;
 
 const handleClick = () => {
   console.log('CLICK');
 };
 
 const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleNavClick = (nav: string) => {
+    navigate(nav);
+    setDrawerOpen(false);
+  };
+
   return (
     <HeaderWrapper>
-      <MaterialIconButton onClick={handleClick} icon={<MdMenu />} />
+      <MaterialIconButton
+        onClick={() => setDrawerOpen(true)}
+        icon={<MdMenu />}
+      />
+      <Drawer
+        anchor={'left'}
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <StyledNav>
+          <button onClick={() => handleNavClick('/')}>Home</button>
+          <Divider />
+          <button onClick={() => handleNavClick('/products')}>
+            Photo Prints
+          </button>
+          <Divider />
+          <button onClick={() => handleNavClick('/user/login')}>Login</button>
+          <button onClick={() => handleNavClick('/user/signup')}>
+            Sign Up
+          </button>
+          <Divider />
+          <button onClick={() => handleNavClick('/about')}>About Us</button>
+          <button onClick={() => handleNavClick('/contact')}>Contact Us</button>
+          <Divider />
+        </StyledNav>
+      </Drawer>
       <Logo src={logo} />
       <FlexWrapper>
         <MaterialIconButton
--- frontend/src/components/styled/Header.styled.ts
+++ frontend/src/components/styled/Header.styled.ts
@@ -15,6 +15,23 @@ export const HeaderWrapper = styled.header`
   box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
+export const StyledNav = styled.nav`
+  width: 250px;
+  background-color: ${colors.white};
+  display: flex;
+  flex-direction: column;
+
+  button {
+    display: flex;
+    align-items: center;
+    background-color: transparent;
+    cursor: pointer;
+    border: none;
+    height: 50px;
+    padding-left: 30px;
+  }
+`;
+
 export const Logo = styled(LogoWrapper)`
   position: absolute;
   left: 50%;
