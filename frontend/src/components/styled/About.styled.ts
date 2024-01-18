56a108533749e36b84b4d0d278ecb6c3dcdf193b
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-18 10:52:45 +0100

    added About Us info

--- frontend/src/components/styled/About.styled.ts
+++ frontend/src/components/styled/About.styled.ts
@@ -1,10 +1,22 @@
-import { colors } from '@/styles/variables';
 import styled from 'styled-components';
 
-export const StyledAbout = styled.div`
-  background-color: ${colors.white};
+export const ImagesContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 350px;
 
   img {
     width: 150px;
+    position: relative;
+
+    &:nth-child(1) {
+      left: 10px;
+    }
+
+    &:nth-child(2) {
+      top: 120px;
+      left: -10px;
+    }
   }
 `;
