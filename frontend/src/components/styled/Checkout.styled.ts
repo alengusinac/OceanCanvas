fa8ee7fe569d2892b8d3b5d6dd3d8219069a462c
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-19 17:04:48 +0100

    added basic checkout component

---
+++ frontend/src/components/styled/Checkout.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const CheckoutForm = styled.form`
+  width: 300px;
+  border: 1px solid black;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+export const CheckoutInput = styled.input`
+  width: 300px;
+`;
--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -1,5 +1,98 @@
+import CartItem from '@/components/CartItem';
+import { CheckoutForm } from '@/components/styled/Checkout.styled';
+import {
+  BodyText,
+  Heading4,
+  SmallBodyText,
+} from '@/components/styled/Text.styled';
+import { Button, Divider } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const items = [
+  {
+    id: 0,
+    title: 'Fish',
+    description: 'This is a lovely fish.',
+    price: 20,
+    size: '20x30',
+    amount: 1,
+    image: 'src/assets/images/ocean1.jpg',
+  },
+  {
+    id: 1,
+    title: 'Shark',
+    description: 'This is a lovely fish.',
+    price: 20,
+    size: '20x30',
+    amount: 2,
+    image: 'src/assets/images/ocean2.jpg',
+  },
+  {
+    id: 2,
+    title: 'Coral',
+    description: 'This is a lovely fish.',
+    price: 20,
+    size: '20x30',
+    amount: 1,
+    image: 'src/assets/images/ocean1.jpg',
+  },
+  {
+    id: 3,
+    title: 'Diver',
+    description: 'This is a lovely fish.',
+    price: 20,
+    size: '20x30',
+    amount: 1,
+    image: 'src/assets/images/ocean2.jpg',
+  },
+];
+
 const Checkout = () => {
-  return <p>Checkout</p>;
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    calculateTotal();
+  });
+
+  const calculateTotal = () => {
+    let totalPrice = 0;
+    items.forEach((item) => {
+      totalPrice += item.amount * item.price;
+    });
+    setTotalPrice(totalPrice);
+
+    setTotalAmount(
+      items.reduce(function (acc, obj) {
+        return acc + obj.amount;
+      }, 0)
+    );
+  };
+
+  return (
+    <>
+      <div>
+        <Heading4>Checkout</Heading4>
+        <Divider />
+        {items.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+        <SmallBodyText>Total quantity: {totalAmount} pcs</SmallBodyText>
+        <BodyText>Total price: ${totalPrice}</BodyText>
+      </div>
+      <Divider />
+      <Heading4>Shipping Information</Heading4>
+      <CheckoutForm>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </CheckoutForm>
+    </>
+  );
 };
 
 export default Checkout;
