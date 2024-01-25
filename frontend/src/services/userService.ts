9c7df526e1f434a62f161daa488254cfa0f35402
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-25 16:33:53 +0100

    added try/catch to axios

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -4,25 +4,41 @@ import { post } from './baseService';
 const BASE_URL = 'http://localhost:3000/users';
 
 export const registerUser = async (values: object) => {
-  const response = await post<IUserResponse>(`${BASE_URL}/register`, values);
-  return response;
+  try {
+    const response = await post<IUserResponse>(`${BASE_URL}/register`, values);
+    return response;
+  } catch (error) {
+    console.log('RegisterError: ', error);
+  }
 };
 
 export const loginUser = async (values: object) => {
-  const response = await post<IUserResponse>(`${BASE_URL}/login`, values);
-  return response;
+  try {
+    const response = await post<IUserResponse>(`${BASE_URL}/login`, values);
+    return response;
+  } catch (error) {
+    console.log('LoginError: ', error);
+  }
 };
 
-export const validateUser = (token: string) => {
-  const response = post<IValidateUserResponse>(`${BASE_URL}/validate`, {
-    token,
-  });
-  return response;
+export const validateUser = async (token: string) => {
+  try {
+    const response = await post<IValidateUserResponse>(`${BASE_URL}/validate`, {
+      token,
+    });
+    return response;
+  } catch (error) {
+    console.log('ValidateError: ', error);
+  }
 };
 
-export const changeUserPassword = (value: string) => {
-  const response = post<IUserResponse>(`${BASE_URL}/change-password`, {
-    newPassword: value,
-  });
-  return response;
+export const changeUserPassword = async (value: string) => {
+  try {
+    const response = await post<IUserResponse>(`${BASE_URL}/change-password`, {
+      newPassword: value,
+    });
+    return response;
+  } catch (error) {
+    console.log('ChangePasswordError: ', error);
+  }
 };
