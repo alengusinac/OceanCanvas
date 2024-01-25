b5a973c9fe16508a298136e1b1342e91ad8668e0
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-25 16:35:01 +0100

    admin authentication

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,7 +1,21 @@
 import Dashboard from '@/components/Dashboard/Dashboard';
+import { useUserContext } from '@/hooks/useUserContext';
+import { useNavigate } from 'react-router-dom';
 
 const Admin = () => {
-  return <Dashboard />;
+  const { user } = useUserContext();
+  console.log('admiN', user);
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      {user?.admin ? (
+        <Dashboard />
+      ) : (
+        <button onClick={() => navigate('/')}>BACK</button>
+      )}
+    </div>
+  );
 };
 
 export default Admin;
