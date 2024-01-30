0ed9a61fd65a57968eeac8f57d17a5feef3fdff1
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-01-30 14:27:54 +0100

    added 2 basic tests

--- frontend/cypress/e2e/1-getting-started/todo.cy.js
+++ frontend/cypress/e2e/1-getting-started/todo.cy.js
@@ -1,9 +1,15 @@
-describe('example to-do app', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:5173/');
+beforeEach(() => {
+  cy.visit('http://localhost:5173/');
+});
+
+describe('find components', () => {
+  it('should display header, main & footer', () => {
+    cy.get('header').should('exist');
+    cy.get('main').should('exist');
+    cy.get('footer').should('exist');
   });
 
-  it('displays hej', () => {
-    cy.get('.hej-text').should('contain', 'HEJ');
+  it('should display logo linking home', () => {
+    cy.get('header a').should('have.attr', 'href', '/');
   });
 });
