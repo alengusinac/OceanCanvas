6e025871843220bdef5ce3f9bda3f3cf57dde336
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-02-06 12:24:00 +0100

    Added ScrollToTop component

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -3,10 +3,12 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { StyledMain } from './styled/Main.styled';
 import { FlexWrapper } from './styled/Flex.styled';
+import ScrollToTop from './ScrollToTop';
 
 const Layout = () => {
   return (
     <FlexWrapper style={{ flexDirection: 'column', minHeight: '100vh' }}>
+      <ScrollToTop />
       <Header />
       <StyledMain>
         <Outlet />
