e6110d803db5b3232c9931e169de893f11a3f87c
Alen Gusinac <61206891+alengusinac@users.noreply.github.com>
2025-10-30 21:00:29 +0100

    Remove backend codebase including models, routes, middleware, services, and configuration files.

--- backend/src/models/CategorySchema.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const CategorySchema = new mongoose.Schema({
-  category: { type: String, required: true },
-  isDeleted: { type: Boolean, default: false },
-});
-
-export const Category = mongoose.model('Categories', CategorySchema);
